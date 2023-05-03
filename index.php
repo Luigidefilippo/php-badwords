321823b39080311170baeaaf9847bcbe24b39f91
Luigidefilippo <defilippoluigi65@gmail.com>
2023-05-03 17:06:17 +0200

    add btn style

--- index.php
+++ index.php
@@ -5,14 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bad Words</title>
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
 <form action="index-word.php">
-  <label for="paragrafo">Inserisci il paragrafo o testo :</label><br>
+  <label for="paragrafo" >Inserisci il paragrafo o testo :</label><br>
   <textarea id="paragrafo" name="paragrafo" rows="4" cols="50"></textarea><br>
   <label for="parola">Inserisci la parola da censurare:</label><br>
-  <input type="text" id="parola" name="parola"><br><br>
-  <input type="submit" value="Invia">
+  <input type="text" id="parola" name="parola" placeholder="inserisci qui la parola
+  "><br><br>
+  <button type="submit">
+    CLICK
+</button>
 </form>
 </body>
 </html>
